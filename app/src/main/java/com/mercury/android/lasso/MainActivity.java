a8e37b43a4393cabf1a3b5e40f71770c5a467714
Caroline <ciacono@mercury.io>
2017-03-01 14:28:01 -0600

    webview works inside of app

--- app/src/main/java/com/mercury/android/lasso/MainActivity.java
+++ app/src/main/java/com/mercury/android/lasso/MainActivity.java
@@ -1,9 +1,18 @@
 package com.mercury.android.lasso;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,44 +25,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView( R.layout.activity_main );
 
     WebView webView = (WebView) findViewById( R.id.webView );
-//    webView.setWebViewClient(new WebViewClient() {
-//        @SuppressWarnings("deprecation") // From API 21 we should use another overload
-//        @Override
-//        public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
-//            return handleRequest(url);
-//        }
-//
-//        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-//        @Override
-//        public WebResourceResponse shouldInterceptRequest(@NonNull WebView view, @NonNull WebResourceRequest request) {
-//            return handleRequest(request.getUrl().toString());
-//        }
-//
-//    private WebResourceResponse handleRequest(String url) {
-//        try {
-//            final Call call = okHttpClient.newCall( new Request.Builder()
-//                                                           .url(url)
-//                                                           // TODO: Add custom headers
-//                                                           .build()
-//                                                  );
-//
-//            final Response response = call.execute();
-//
-//            // TODO: Probably need to go back to the UI thread, verify first
-//            // TODO: Get the title from some custom "x-" header instead of using the response code
-//            updateScreenTitle(Integer.toString(response.code()));
-//
-//            return new WebResourceResponse(
-//                    response.header("content-type", "text/plain"), // You can set something other as default content-type
-//                    response.header("content-encoding", "utf-8"),  // Again, you can set another encoding as default
-//                    response.body().byteStream()
-//            );
-//        } catch (Exception e) {
-//            // TODO: Figure out how to show a custom error screen when we fail
-//            return null;
-//        }
-//    }
-//});
+    webView.setWebViewClient(new WebViewClient() {
+        @SuppressWarnings("deprecation") // From API 21 we should use another overload
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+            return handleRequest(url);
+        }
+
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public WebResourceResponse shouldInterceptRequest(@NonNull WebView view, @NonNull WebResourceRequest request) {
+            return handleRequest(request.getUrl().toString());
+        }
+
+    private WebResourceResponse handleRequest(String url) {
+        try {
+            final Call call = okHttpClient.newCall( new Request.Builder()
+                                                           .url(url)
+                                                           // TODO: Add custom headers
+                                                           .build()
+                                                  );
+
+            final Response response = call.execute();
+
+            // TODO: Probably need to go back to the UI thread, verify first
+            // TODO: Get the title from some custom "x-" header instead of using the response code
+            updateScreenTitle(Integer.toString(response.code()));
+
+            return new WebResourceResponse(
+                    response.header("content-type", "text/plain"), // You can set something other as default content-type
+                    response.header("content-encoding", "utf-8"),  // Again, you can set another encoding as default
+                    response.body().byteStream()
+            );
+        } catch (Exception e) {
+            // TODO: Figure out how to show a custom error screen when we fail
+            return null;
+        }
+    }
+});
 
         webView.loadUrl("http://www.google.com");
         }
